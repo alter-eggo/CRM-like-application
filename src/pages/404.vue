2601a19dbba25466de52c6091dddc5b2e955c048
Anastas <anastas3993@gmail.com>
2019-04-18 11:08:02 +0300

    1 сommit
    added routing and axios, table of users, get users by api, sorting of users

---
+++ src/pages/404.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+    <div class="wrapper-content wrapper-content--fixed">
+      <section>
+        <div class="container">
+          <h1 class="title">Page not found</h1>
+          <router-link to="/" class="link">Go to main page?</router-link>
+
+        </div>
+      </section>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+.wrapper-content{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 80vh;
+  text-align: center;
+}
+</style>
